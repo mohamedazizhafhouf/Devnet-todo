1cfd218f00b6c9b90ef06735f4e3a2dad207ab32
Mohamed Aziz Hafhouf <74309877+mohamedazizhafhouf@users.noreply.github.com>
2022-01-30 17:42:00 +0100

    update editProjectController
    1 - Preparing the get method

--- todo/app/Http/Controllers/editProjectController.php
+++ todo/app/Http/Controllers/editProjectController.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\DB;
+
 
 use Illuminate\Http\Request;
 
@@ -8,12 +10,38 @@ class editProjectController extends Controller
 {
     //
 
-    /*function getProjectDetails($id){
-        $query=
-    }*/
+    function getProjectDetails($id){
+        $projectDetails= DB::table('project')
+        ->where('idProject',$id)
+        ->get();
+        return $projectDetails;
+    }
+
+    function getListMembers($id){
+        $listMembers = DB::table('project')
+            ->join('user_project','project.idProject',"=","user_project.idProject")
+            ->join('users','user_project.idUser',"=","users.id")
+            ->select('users.name')
+            ->where('project.idProject','=',$id)
+            ->get();
+        return $listMembers;
+    }
+
+    function getListNotMembers($id){
+        $listNotMembers = DB::table('users')
+        ->select('id','name')
+        ->whereNotIn('users.id',function($query){
+            $query->select('idUser')  
+            ->from('user_project');
+            })->get();
+        
+        return $listNotMembers;
+    }    
 
     function get($id){
-        //$details=getProjectDetails($id);
+        $details=$this->getProjectDetails($id);
+        $listMembers=$this->getListMembers($id);
+        $listNotMembers=$this->getListNotMembers($id);
         //return view('editProject');
     }
 }
